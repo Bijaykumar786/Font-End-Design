ba6ec0e225f7d42e562ce9a1206b76913e43a444
Bijaykumar786 <vijay786das@gmail.com>
2025-02-13 10:24:35 +0545

    add the js

---
+++ 2nd.js
@@ -0,0 +1,45 @@
+// // using string 
+
+// let str1="45";
+// let str2="World";
+// let str3=`Welcome, ${str1} !`;
+// console.log(str3);
+// console.log(str1.length);
+// console.log(str1.toUpperCase());
+// console.log(typeof str1);
+
+// let num1=45;
+// let num2=3.14;
+// console.log(num1+num2);
+// console.log(num1/0);
+// console.log(isNaN("Hello"/2));
+
+
+// let is = true;
+// let isfalse = false;
+// console.log(isTrue && isFalse);
+// console.log(isTrue || isFalse);
+
+
+// Type converison
+
+// console.log("5" + 5); // string concatenation
+// console.log("5" - 5); // string is converted to number
+// console.log("5" * 5); // 
+// console.log("5" / 5);
+// console.log(true + 1); // true is converted to 1
+// console.log(false + 1);
+// console.log(true - 1);
+// console.log(false - 1);
+
+
+let str = "123";
+let num = Number(str); //convertng string to number
+console.log(num);
+
+let num2 = 456;
+let str2 = String(num2); // converting number to string
+console.log(str2);
+
+console.log(Boolean(0)); // false
+console.log(Boolean(123)); // none is zero so true
